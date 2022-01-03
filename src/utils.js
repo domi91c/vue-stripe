429497780f4ccce7d6efde1f7c9ca383a341651e
Dominic Nunes <dominic.n@me.com>
2022-01-03 05:14:26 -0500

    Add localtest.me as development host

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 export const isSecureHost = () => {
   return (
+    window.location.hostname.match(/.*localtest.me/) ||
     window.location.hostname.match(/.*lvh.me/) ||
     window.location.hostname === 'localhost' ||
     window.location.protocol === 'https:'
